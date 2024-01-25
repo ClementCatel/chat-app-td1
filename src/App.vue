8f9afa0660773d59b06a9fa87185e7303aeea851
Clement Catel <clement.catel4@gmail.com>
2024-01-25 10:58:56 +0100

    feat: add message input

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 
 <template>
-  <div>navbar</div>
-  <router-view></router-view>
+  <div class="bg-gray-800 h-full text-white">
+    <router-view></router-view>
+  </div>
 </template>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,3 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const messageContent = ref('')
+const messages = ref([])
+
+const addMessage = () => {
+  messages.value.push(messageContent.value)
+  messageContent.value = ''
+}
+</script>
+
 <template>
-  <div class="bg-red-600">Hello World!</div>
+  <div class="p-4">
+    <div v-for="(message, index) in messages" :key="index">
+      {{ message }}
+    </div>
+  </div>
+
+  <div class="flex p-4">
+    <textarea
+      v-model="messageContent"
+      name="message"
+      id="message"
+      rows="1"
+      class="text-black"
+    ></textarea>
+    <button @click="addMessage" class="rounded-md bg-blue-500 p-3 ml-3">Envoyer</button>
+  </div>
 </template>
